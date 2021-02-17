c9cd8de5c78ba0d1099051fa4c9a313dd179ce45
Judit Mendez Puig <mendez.puig@gmail.com>
2021-02-17 02:41:23 +0000

    add cookies for log in/logout functionality

--- express_server.js
+++ express_server.js
@@ -1,27 +1,27 @@
 const express = require("express");
-const app = express();
 const bodyParser = require("body-parser");
 var morgan = require('morgan')
-app.use(morgan('dev'))
+const cookieParser = require('cookie-parser')
 
+const app = express();
 const PORT = 8080; // default port 8080
 
+////// USE
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(morgan('dev'))
+app.use(cookieParser())
+
+////// SET
+app.set("view engine", "ejs");
 
 const generateRandomString = function() {
   return Math.floor((1 + Math.random()) * 0x1000000).toString(16).substring(1);
 };
 
-
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
-
-////// USE
-app.use(bodyParser.urlencoded({extended: true}));
-
-////// SET
-app.set("view engine", "ejs");
 
 ////// GET
 app.get("/", (req, res) => {
@@ -29,16 +29,26 @@ app.get("/", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { 
+    username: req.cookies["username"],
+    urls: urlDatabase 
+  };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = { 
+    username: req.cookies["username"],
+    urls: urlDatabase 
+  };
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  const templateVars = { 
+    username: req.cookies["username"],
+    shortURL: req.params.shortURL, 
+    longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
 
@@ -55,6 +65,16 @@ app.post("/urls", (req, res) => {
   urlDatabase[shortURL] = longURL;
   res.redirect(`/urls/${shortURL}`);
 });
+
+app.post('/login', function (req, res) {
+  res.cookie("username", req.body.username)
+  res.redirect("/urls");
+})
+
+app.post('/logout', function (req, res) {
+  res.clearCookie("username");
+  res.redirect("/urls");
+})
 
 app.post("/urls/:id", (req, res) => {
   const shortURL = req.params.id;
